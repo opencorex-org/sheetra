e2af891b5c51ee92e53890d9e21a08a225bfbe97
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2026-02-28 04:36:07 +0530

    Add Rollup configuration for TypeScript build process

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+
+const typescript = require('@rollup/plugin-typescript');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const dts = require('rollup-plugin-dts').default;
+const pkg = require('./package.json');
+
+module.exports = [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named',
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+    external: [],
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: pkg.types, format: 'es' }],
+    plugins: [dts()],
+  },
+];
